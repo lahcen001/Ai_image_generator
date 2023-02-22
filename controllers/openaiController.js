5a585c06088804596710b9e748914365ab24f02d
Lahcen001 <lahcen0000lh@gmail.com>
2023-02-22 01:16:46 -0600

    first commit 1

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -6,12 +6,19 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
+
 const generateImage = async(req, res )=>{
+
+  var {prompt, size} = req.body;
+
+ const imageSize = size === 'small' ? '256x256':size === 'medium' ? '512x512':'1024x1024';
+
+
   try {
     const response = await openai.createImage({
-        prompt:'man and women',
+        prompt,
         n:1,
-        size:'512x512'
+        size:imageSize
     });
     const imageUrl = response.data.data[0].url
 
